dc7a879760809ce2c0aa3692a50feed36f76fd2f
todanh2706 <thdanh23@clc.fitus.edu.vn>
2025-12-16 19:49:33 +0700

    code: Add src folder with dump files

---
+++ src/remote-directory-monitor/monitor-common/src/main/java/com/danh/common/Message.java
@@ -0,0 +1,7 @@
+package com.danh.common;
+
+import java.io.Serializable;
+
+public class Message implements Serializable {
+    public String text = "Hello from Common";
+}
---
+++ src/remote-directory-monitor/monitor-server/src/main/java/com/danh/server/ServerApp.java
@@ -0,0 +1,9 @@
+package com.danh.server;
+
+import com.danh.common.Message;
+
+public class ServerApp {
+    public static void main(String[] args) {
+
+    }
+}
